f4cb0f63a99cc32cbae077c9784c8a1f038ab169
hinajawaid <hinajawaid.h@gmail.com>
2025-03-25 18:04:14 +0100

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        JavaBasics basicObj = new JavaBasics();
+        //    basicObj.basics();
+        ConditionsUndMethods cUmObj = new ConditionsUndMethods();
+        cUmObj.coditions();
+        String name1 = "Hina";
+        String name2 = "Jawaid";
+        cUmObj.greetStudent(name1);
+        //isEven
+        System.out.println(("Number is Even: "+cUmObj.isEven(20)));
+        System.out.println(("Number is Even: "+cUmObj.isEven(20)));//testing updates
+        System.out.println("multiple value: "+cUmObj.multiply(2,3));
+        System.out.println("Subtracted value: "+cUmObj.add(10.343f,7.7828f));
+        System.out.println("Subtracted value: "+cUmObj.subtract(10,7));
+        //makePositvnumber
+        System.out.println("Positive als positiv: "+cUmObj.makePositiv(10));
+        System.out.println("Negativ als positiv: "+cUmObj.makePositiv(-9));
+    }
+}
